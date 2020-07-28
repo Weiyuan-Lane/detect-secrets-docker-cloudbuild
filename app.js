9aef3bb267b7832db892864473fccdc13129f1b4
Weiyuan Liu <weiyuan.lane@gmail.com>
2020-07-29 00:06:24 +0800

    [Ops] Update app.js (let's break it again!)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var http = require('http');
 
 API_KEY = "blah-blah-but-actually-not-secret-yes"
 
+ANOTHER_API_KEY = "12xkasasd,m-1232cal;sd"
+
 http.createServer(function (_, res) {
   res.write('Hello World!'); 
   res.end();
