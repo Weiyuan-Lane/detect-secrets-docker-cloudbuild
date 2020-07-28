28129e86ac7677380ff6c77418a6682e048e2b14
Weiyuan Liu <weiyuan.lane@gmail.com>
2020-07-29 00:09:36 +0800

    [Ops] Nonsense update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var http = require('http');
 
 API_KEY = "blah-blah-but-actually-not-secret-yes"
 
+console.log('cats');
+
 http.createServer(function (_, res) {
   res.write('Hello World!'); 
   res.end();
